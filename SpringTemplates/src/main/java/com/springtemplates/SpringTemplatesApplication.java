90576e63f1b2122186823a3205144146b097f294
SpringTemplates <springtemplates@gmail.com>
2018-05-17 16:04:23 +0700

    Start First 18052018

---
+++ SpringTemplates/src/main/java/com/springtemplates/SpringTemplatesApplication.java
@@ -0,0 +1,12 @@
+package com.springtemplates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplatesApplication.class, args);
+	}
+}
---
+++ SpringTemplates/src/test/java/com/springtemplates/SpringTemplatesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springtemplates;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTemplatesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
